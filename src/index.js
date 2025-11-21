1438e899537362678532d71f6e6d973c5c5dced8
Iris Cheung <hokyinirischeung@gmail.com>
2025-11-21 09:05:10 -0800

    added count crab

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-const addCrab = () => {
+const state = {
+  crabCount: 0,
+};
+
+const addCrab = (event) => {
+  console.log('in addCrab:', event);
   const newCrab = document.createElement('span');
   const crabContainer = document.querySelector('#crabContainer');
   newCrab.textContent = '🦀';
   crabContainer.appendChild(newCrab);
+  state.crabCount += 1;
+  const crabCountContainer = document.querySelector('#crabCount');
+  crabCountContainer.textContent = `Crab Count: ${state.crabCount}`;
 };
 
-const registerEventHandlers = () => {
+const registerEventHandlers = (event) => {
+  console.log('in registerEventHandlers:', event);
   const crabButton = document.querySelector('#addCrabButton');
   crabButton.addEventListener('click', addCrab);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
+
